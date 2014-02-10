eeb560e82f037bd88fd1893bb154a63eb7bb3123
Spencer Carli <spencercarli@gmail.com>
2014-02-09 23:15:18 -0500

    Added an array of items in the fixtures file

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,5 +1,8 @@
 if (Lists.find().count() === 0){
-	Lists.insert({title: "Title 1"});
+	Lists.insert({
+		title: "Title 1",
+		items: ["item 1", "item 2", "item 3"]
+	});
 	Lists.insert({title: "Title 2"});
 	Lists.insert({title: "Title 3"});
 }
