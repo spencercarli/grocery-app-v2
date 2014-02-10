65d18723551589f3942c09ab49064a85a6273797
Spencer Carli <spencercarli@gmail.com>
2014-02-10 11:36:39 -0500

    Form to add a new item to a list

--- client/views/dashboard/newListItem.js
+++ client/views/dashboard/newListItem.js
@@ -2,6 +2,14 @@ Template.newListItem.events({
 	'click #submit' : function(e) {
 		e.preventDefault();
 		var x = $('[name=newItem]').val();
-		
+		 
+		var item = {
+			item: x,
+			purchased: false,
+			listId: this._id,
+			submitted: new Date();
+		}
+
+		Items.insert(item);
 	}
 });
