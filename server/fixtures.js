a00ce33bd4b0c5fdd441461c4d8d8bbd871cc22f
Spencer Carli <spencercarli@gmail.com>
2014-02-10 08:40:36 -0500

    List out items belonging to a list

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,7 +1,17 @@
 if (Lists.find().count() === 0){
 	Lists.insert({
 		title: "Title 1",
-		items: ["item 1", "item 2", "item 3"]
+		items: [
+			{
+				item: 'item 1'
+			},
+			{
+				item: 'item 2'
+			},
+			{
+				item: 'item 3'
+			}
+		]
 	});
 	Lists.insert({title: "Title 2"});
 	Lists.insert({title: "Title 3"});
