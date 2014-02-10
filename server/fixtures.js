10eaf941f537c0f3fbf6758b0a20401c2bf79d5e
Spencer Carli <spencercarli@gmail.com>
2014-02-09 22:46:41 -0500

    Setup pub/sub for lists and some fixture data

---
+++ server/fixtures.js
@@ -0,0 +1,5 @@
+if (Lists.find().count() === 0){
+	Lists.insert({title: "Title 1"});
+	Lists.insert({title: "Title 2"});
+	Lists.insert({title: "Title 3"});
+}
---
+++ server/publications.coffee
@@ -0,0 +1,2 @@
+Meteor.publish 'lists', ->
+	Lists.find();
