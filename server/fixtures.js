85f30686130861ecdd4b81fb75dc6311c75463ad
Spencer Carli <spencercarli@gmail.com>
2014-02-14 12:15:06 -0500

    Hide the fixture data

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,32 +1,32 @@
-Meteor.startup(function(){
-	if (Lists.find().count() === 0){
-		var x = Lists.insert({
-			title: "Title 1",
-			submitted: new Date()
-		});
-		Lists.insert({
-			title: "Title 2",
-			submitted: new Date()
-		});
-		Lists.insert({
-			title: "Title 3",
-			submitted: new Date()
-		});
+// Meteor.startup(function(){
+// 	if (Lists.find().count() === 0){
+// 		var x = Lists.insert({
+// 			title: "Title 1",
+// 			submitted: new Date()
+// 		});
+// 		Lists.insert({
+// 			title: "Title 2",
+// 			submitted: new Date()
+// 		});
+// 		Lists.insert({
+// 			title: "Title 3",
+// 			submitted: new Date()
+// 		});
 
-		Items.insert({
-			item: 'item 1 for title 1',
-			purchased: false,
-			needed: true,
-			listId : x,
-			submitted: new Date()
-		});
+// 		Items.insert({
+// 			item: 'item 1 for title 1',
+// 			purchased: false,
+// 			needed: true,
+// 			listId : x,
+// 			submitted: new Date()
+// 		});
 
-		Items.insert({
-			item: 'item 2 for title 1',
-			purchased: false,
-			needed: true,
-			listId : x,
-			submitted: new Date()
-		});
-	}
-});
+// 		Items.insert({
+// 			item: 'item 2 for title 1',
+// 			purchased: false,
+// 			needed: true,
+// 			listId : x,
+// 			submitted: new Date()
+// 		});
+// 	}
+// });
