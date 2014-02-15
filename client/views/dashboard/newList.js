69236b46b7cf0b1aff8c21bb89462143488d38d8
Spencer Carli <spencercarli@gmail.com>
2014-02-15 18:04:01 -0500

    Cleaned up a file and added some instructions

--- client/views/dashboard/newList.js
+++ client/views/dashboard/newList.js
@@ -8,8 +8,6 @@ Template.newList.events({
 			title: name.val(),
 			owner: Meteor.user()._id,
 			sharedWith: [Meteor.user()._id]
-			// sharedWith array that we loop over and check if the user is one of the people the list is shared with
-			// sharedWith : this.forEach() -- check params for forEach for js 
 		}
 
 		list._id = Lists.insert(list);
